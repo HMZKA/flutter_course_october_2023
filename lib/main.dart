063475fb830a57ae164520eb13c17421b5ba13ad
HMZKA <hmzakh65@gmail.com>
2023-10-29 15:55:21 +0200

    row

--- lib/main.dart
+++ lib/main.dart
@@ -14,49 +14,24 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
           appBar: AppBar(title: Text("My Application")),
           body: Container(
-            width: double.infinity,
-            color: Colors.black,
-            child: Column(
-              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              // crossAxisAlignment: CrossAxisAlignment.center,
+            height: double.infinity,
+            color: Colors.blue,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                // 1 + 1 + 1 + 1 / 4
-                Expanded(
-                  flex: 3,
-                  child: Container(
-                    color: Colors.red,
-                    child: Text(
-                      "First Text",
-                      style: TextStyle(fontSize: 20),
-                    ),
+                Container(
+                  color: Colors.red,
+                  child: Text(
+                    "First Text",
+                    style: TextStyle(fontSize: 20),
                   ),
                 ),
-                Expanded(
-                  flex: 3,
-                  child: Container(
-                    color: Colors.amber,
-                    child: Text(
-                      "Second Text",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    color: Colors.green,
-                    child: Text(
-                      "Third Text",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    color: Colors.purple,
-                    child: Text(
-                      "Fourth Text",
-                      style: TextStyle(fontSize: 20),
-                    ),
+                Container(
+                  color: Colors.green,
+                  child: Text(
+                    "Second Text",
+                    style: TextStyle(fontSize: 20),
                   ),
                 ),
               ],
